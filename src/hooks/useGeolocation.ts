f26e94755a3c02baf4b4a0982fb56295a7f55be2
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-10-22 11:29:18 +0200

    feat: add useGeolocation hook
    * This commit introduces a new `useGeolocation` hook to abstract the logic for retrieving the user's geolocation.
    * The hook leverages the browser's `navigator.geolocation` API.
    * It handles success and error scenarios, including cases where geolocation is not supported by the browser.
    * Exposes the location state which contains the latitute, longitude, error and loading properties.

---
+++ src/hooks/useGeolocation.ts
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface LocationState {
+  latitude: number | null;
+  longitude: number | null;
+  error: string | null;
+  loading: boolean;
+}
+
+export default function useGeolocation() {
+  const [location, setLocation] = useState<LocationState>({
+    latitude: null,
+    longitude: null,
+    error: null,
+    loading: true,
+  });
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setLocation({
+        latitude: null,
+        longitude: null,
+        error: "Geolocation not supported",
+        loading: false,
+      });
+      return;
+    }
+
+    const onSuccess = (position: GeolocationPosition) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        error: null,
+        loading: false,
+      });
+    };
+
+    const onError = (error: GeolocationPositionError) => {
+      setLocation({
+        latitude: null,
+        longitude: null,
+        error: error.message,
+        loading: false,
+      });
+    };
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0,
+    });
+  }, []);
+
+  return location;
+}
