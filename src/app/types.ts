3e742a24b31607035e5efdd30541528cb8c5404b
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-11-26 17:40:04 +0100

    feat: add more weather parameter and unit types

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,20 +2,66 @@
 /*                                  SMHI API                                  */
 /* -------------------------------------------------------------------------- */
 
-import WeatherHourCard from "@/components/ui/WeatherHourCard";
+export type WeatherParameterName =
+  | "t"
+  | "wd"
+  | "ws"
+  | "gust"
+  | "r"
+  | "msl"
+  | "vis"
+  | "tstm"
+  | "tcc_mean"
+  | "lcc_mean"
+  | "mcc_mean"
+  | "hcc_mean"
+  | "pmean"
+  | "pmin"
+  | "pmax"
+  | "pmedian"
+  | "spp"
+  | "pcat"
+  | "Wsymb2"
+  | "tp";
 
-export const WEATHER_PARAMETERS = {
-  t: "Temperature",
-  ws: "Wind Speed",
-  r: "Relative Humidity",
-  msl: "Air Pressure",
-  Wsymb2: "Weather Symbol",
-  vis: "Visibility",
-} as const;
+export type WeatherParameterUnit =
+  | "Cel"
+  | "degree"
+  | "m/s"
+  | "percent"
+  | "hPa"
+  | "octas"
+  | "kg/m2/h"
+  | "category"
+  | "kg/m2";
+
+export const WeatherParametersFullNames: Record<WeatherParameterName, string> =
+  {
+    t: "Temperature",
+    wd: "Wind direction",
+    ws: "Wind speed",
+    gust: "Wind gust",
+    r: "Relative humidity",
+    msl: "Sea level pressure",
+    vis: "Visibility",
+    tstm: "Thunderstorm",
+    tcc_mean: "Cloud cover",
+    lcc_mean: "Low cloud cover",
+    mcc_mean: "Medium cloud cover",
+    hcc_mean: "High cloud cover",
+    pmean: "Mean precipitation",
+    pmin: "Minimum precipitation",
+    pmax: "Maximum precipitation",
+    pmedian: "Median precipitation",
+    spp: "Snow precipitation",
+    pcat: "Precipitation category",
+    Wsymb2: "Weather symbol",
+    tp: "Total precipitation",
+  };
 
 export interface WeatherParameter {
-  name: string;
-  unit: string;
+  name: WeatherParameterName;
+  unit: WeatherParameterUnit;
   values: number[];
 }
 
@@ -42,7 +88,7 @@ export interface SMHIWeatherData {
 
 export interface WeatherDay {
   date: string;
-  timeSeries: WeatherHour[];
+  hours: WeatherHour[];
 }
 
 export interface WeatherWeek {
