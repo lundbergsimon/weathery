b9c92b95f1ca053ccb9676a23d146dda49f14808
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-11-26 17:37:30 +0100

    feat: add HourlyWeatherRow component to display hourly weather data

---
+++ src/components/hourly-weather-row.tsx
@@ -0,0 +1,38 @@
+import { WeatherParameter } from "@/app/types";
+
+interface HourlyWeatherRowProps {
+  data: { hour: Date; parameters: WeatherParameter[] }[];
+  isExpanded: boolean;
+}
+
+export default function HourlyWeatherRow({
+  data,
+  isExpanded,
+}: HourlyWeatherRowProps) {
+  return (
+    <div className="pb-4">
+      {/* Display each hour horizontally */}
+      <div className="flex gap-4">
+        {data.map((item, index) => (
+          <div key={index} className="text-center cursor-default">
+            <p className="text-xs font-bold text-text-muted">
+              {new Date(item.hour).toLocaleString("en-US", {
+                hour: "numeric",
+                hour12: false,
+                minute: "numeric",
+              })}
+            </p>
+            <p className="font-bold">{item.parameters[0].values[0]}°</p>
+            {isExpanded && (
+              <div>
+                <p className="text-xs text-text-muted">
+                  {item.parameters[1].values[0]}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
