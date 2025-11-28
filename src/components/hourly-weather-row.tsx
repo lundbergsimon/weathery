0d332bd234b6ceb2931847c97a1992c2611f3031
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-11-28 11:57:33 +0100

    feat(hourly-weather-row): display weather symbol

--- src/components/hourly-weather-row.tsx
+++ src/components/hourly-weather-row.tsx
@@ -1,3 +1,4 @@
+import { SMHI_WEATHER_SYMBOLS } from "@/constants/mesan";
 import { WeatherParameter } from "@/types";
 
 interface HourlyWeatherRowProps {
@@ -13,25 +14,32 @@ export default function HourlyWeatherRow({
     <div className="pb-4">
       {/* Display each hour horizontally */}
       <div className="flex gap-4">
-        {data.map((item, index) => (
-          <div key={index} className="text-center cursor-default">
-            <p className="text-xs font-bold text-text-muted">
-              {new Date(item.hour).toLocaleString("en-US", {
-                hour: "numeric",
-                hour12: false,
-                minute: "numeric",
-              })}
-            </p>
-            <p className="font-bold">{item.parameters[0].values[0]}°</p>
-            {isExpanded && (
-              <div>
-                <p className="text-xs text-text-muted">
-                  {item.parameters[1].values[0]}
-                </p>
-              </div>
-            )}
-          </div>
-        ))}
+        {data.map((item, index) => {
+          const symbolValue = Number(
+            item.parameters.find((p) => p.name === "Wsymb2")!.values[0]
+          );
+          const WeatherSymbol = SMHI_WEATHER_SYMBOLS[symbolValue];
+          return (
+            <div key={index} className="text-center cursor-default">
+              <p className="text-xs font-bold text-text-muted">
+                {new Date(item.hour).toLocaleString("en-US", {
+                  hour: "numeric",
+                  hour12: false,
+                  minute: "numeric",
+                })}
+              </p>
+              <p className="font-bold">{item.parameters[0].values[0]}°</p>
+              <span className="text-text-muted flex justify-center text-2xl">{<WeatherSymbol.icon />}</span>
+              {isExpanded && (
+                <div>
+                  <p className="text-xs text-text-muted">
+                    {item.parameters[1].values[0]}
+                  </p>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
