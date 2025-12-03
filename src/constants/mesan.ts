dd9ba8d2e6ecd7ec44616da4e1b3c7ab48ed567f
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-12-03 10:41:30 +0100

    refactor: modify "icon" field to any object with day and night fields

--- src/constants/mesan.ts
+++ src/constants/mesan.ts
@@ -2,10 +2,20 @@ import { IconType } from "react-icons/lib";
 import {
   WiCloud,
   WiDayCloudy,
+  WiDayFog,
+  WiDayRain,
   WiDaySunny,
   WiDaySunnyOvercast,
-  WiFog,
   WiLightning,
+  WiNightClear,
+  WiNightCloudy,
+  WiNightFog,
+  WiNightLightning,
+  WiNightPartlyCloudy,
+  WiNightRain,
+  WiNightSleet,
+  WiNightSnow,
+  WiNightThunderstorm,
   WiRain,
   WiShowers,
   WiSleet,
@@ -17,34 +27,115 @@ export const SMHI_WEATHER_SYMBOLS: Record<
   number,
   {
     label: string;
-    icon: IconType;
+    icon: { day: IconType; night: IconType };
   }
 > = {
-  1: { label: "Clear sky", icon: WiDaySunny },
-  2: { label: "Nearly clear sky", icon: WiDaySunnyOvercast },
-  3: { label: "Variable cloudiness", icon: WiDayCloudy },
-  4: { label: "Halfclear sky", icon: WiDayCloudy },
-  5: { label: "Cloudy sky", icon: WiCloud },
-  6: { label: "Overcast", icon: WiCloud },
-  7: { label: "Fog", icon: WiFog },
-  8: { label: "Light rain showers", icon: WiRain },
-  9: { label: "Moderate rain showers", icon: WiShowers },
-  10: { label: "Heavy rain showers", icon: WiShowers },
-  11: { label: "Thunderstorm", icon: WiLightning },
-  12: { label: "Light sleet showers", icon: WiSleet },
-  13: { label: "Moderate sleet showers", icon: WiSleet },
-  14: { label: "Heavy sleet showers", icon: WiSleet },
-  15: { label: "Light snow showers", icon: WiSnow },
-  16: { label: "Moderate snow showers", icon: WiSnow },
-  17: { label: "Heavy snow showers", icon: WiSnow },
-  18: { label: "Light rain", icon: WiRain },
-  19: { label: "Moderate rain", icon: WiRain },
-  20: { label: "Heavy rain", icon: WiRain },
-  21: { label: "Thunder", icon: WiThunderstorm },
-  22: { label: "Light sleet", icon: WiSleet },
-  23: { label: "Moderate sleet", icon: WiSleet },
-  24: { label: "Heavy sleet", icon: WiSleet },
-  25: { label: "Light snowfall", icon: WiSnow },
-  26: { label: "Moderate snowfall", icon: WiSnow },
-  27: { label: "Heavy snowfall", icon: WiSnow },
+  1: {
+    label: "Clear sky",
+    icon: { day: WiDaySunny, night: WiNightClear },
+  },
+  2: {
+    label: "Nearly clear sky",
+    icon: { day: WiDaySunnyOvercast, night: WiNightPartlyCloudy },
+  },
+  3: {
+    label: "Variable cloudiness",
+    icon: { day: WiDayCloudy, night: WiNightCloudy },
+  },
+  4: {
+    label: "Halfclear sky",
+    icon: { day: WiDayCloudy, night: WiNightCloudy },
+  },
+  5: {
+    label: "Cloudy sky",
+    icon: { day: WiCloud, night: WiNightCloudy },
+  },
+  6: {
+    label: "Overcast",
+    icon: { day: WiCloud, night: WiCloud },
+  },
+  7: {
+    label: "Fog",
+    icon: { day: WiDayFog, night: WiNightFog },
+  },
+  8: {
+    label: "Light rain showers",
+    icon: { day: WiRain, night: WiNightRain },
+  },
+  9: {
+    label: "Moderate rain showers",
+    icon: { day: WiShowers, night: WiShowers },
+  },
+  10: {
+    label: "Heavy rain showers",
+    icon: { day: WiShowers, night: WiShowers },
+  },
+  11: {
+    label: "Thunderstorm",
+    icon: { day: WiLightning, night: WiNightLightning },
+  },
+  12: {
+    label: "Light sleet showers",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  13: {
+    label: "Moderate sleet showers",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  14: {
+    label: "Heavy sleet showers",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  15: {
+    label: "Light snow showers",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
+  16: {
+    label: "Moderate snow showers",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
+  17: {
+    label: "Heavy snow showers",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
+  18: {
+    label: "Light rain",
+    icon: { day: WiDayRain, night: WiNightRain },
+  },
+  19: {
+    label: "Moderate rain",
+    icon: { day: WiRain, night: WiRain },
+  },
+  20: {
+    label: "Heavy rain",
+    icon: { day: WiRain, night: WiRain },
+  },
+  21: {
+    label: "Thunder",
+    icon: { day: WiThunderstorm, night: WiNightThunderstorm },
+  },
+  22: {
+    label: "Light sleet",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  23: {
+    label: "Moderate sleet",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  24: {
+    label: "Heavy sleet",
+    icon: { day: WiSleet, night: WiNightSleet },
+  },
+  25: {
+    label: "Light snowfall",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
+  26: {
+    label: "Moderate snowfall",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
+  27: {
+    label: "Heavy snowfall",
+    icon: { day: WiSnow, night: WiNightSnow },
+  },
 };
