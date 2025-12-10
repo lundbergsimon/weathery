a2ba33c9e61cc191509195826393779af951a912
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-12-10 12:12:37 +0100

    test: remove any archive folder from tests

--- jest.config.ts
+++ jest.config.ts
@@ -170,9 +170,9 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    ".*archive.*",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
