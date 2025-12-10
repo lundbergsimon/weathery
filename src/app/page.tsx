ba758bde9e5da8920b206b68715c42d033ff9ca8
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-12-10 12:48:56 +0100

    refactor: move day-weather-component to new file
    - Refactor the code to a new file
    - Renames the component from `DayComponent` to `DayWeatherComponent`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import Card from "@/components/card";
 import CurrentWeatherCard from "@/components/current-weather-card";
-import HorizontalScrollContainer from "@/components/horizontal-scroll-container";
-import HourlyWeatherRow from "@/components/hourly-weather-row";
+import DayWeatherComponent from "@/components/day-weather-component";
 import ErrorState from "@/components/ui/ErrorState";
 import LoadingState from "@/components/ui/LoadingState";
 import useGeoLocation from "@/hooks/useGeolocation";
 import useWeather from "@/hooks/useWeather";
-import { WeatherDay } from "@/types/index";
-import { displayMonthDay, displayWeekDay } from "@/utils/helpers";
 
 /**
  * A page that displays the current weather data for a given location.
@@ -49,7 +45,9 @@ export default function WeatherPage() {
             className="flex flex-col gap-4 pt-4 justify-center max-w-fit min-w-0"
           >
             {weeks.map((week) =>
-              week.days.map((day) => <DayComponent key={day.date} day={day} />)
+              week.days.map((day) => (
+                <DayWeatherComponent key={day.date} day={day} />
+              ))
             )}
           </section>
         </div>
@@ -60,51 +58,5 @@ export default function WeatherPage() {
         </p>
       </footer>
     </>
-  );
-}
-
-/* -------------------------------------------------------------------------- */
-/*                                DayComponent                                */
-/* -------------------------------------------------------------------------- */
-
-interface DayComponentProps {
-  day: WeatherDay;
-}
-
-function DayComponent({ day }: DayComponentProps) {
-  const temperatureLow = day.hours.reduce(
-    (acc, hour) =>
-      Math.min(acc, hour.parameters.find((p) => p.name === "t")!.values[0]!),
-    Infinity
-  );
-  const temperatureHigh = day.hours.reduce(
-    (acc, hour) =>
-      Math.max(acc, hour.parameters.find((p) => p.name === "t")!.values[0]!),
-    -Infinity
-  );
-
-  return (
-    <div className="" key={day.date}>
-      <h2 className="text-2xl font-bold mb-1 px-1 flex justify-between">
-        <span>{displayWeekDay(day)}</span>
-        <span>{displayMonthDay(day)}</span>
-      </h2>
-      <Card>
-        <div className="text-text-main text-lg mb-4">
-          {temperatureLow.toFixed(0)}° / {temperatureHigh.toFixed(0)}°
-        </div>
-        <HorizontalScrollContainer>
-          <HourlyWeatherRow
-            data={day.hours.map((hour) => ({
-              hour: new Date(hour.validTime),
-              parameters: hour.parameters.map((param) => ({
-                ...param,
-                values: param.values.map((value) => Number(value.toFixed(0))),
-              })),
-            }))}
-          />
-        </HorizontalScrollContainer>
-      </Card>
-    </div>
   );
 }
