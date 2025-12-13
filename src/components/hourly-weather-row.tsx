c7c045c80a4099138606b22bf22f4d9142af4010
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-12-13 22:17:30 +0100

    refactor(HourlyWeatherRow): improve readability

--- src/components/hourly-weather-row.tsx
+++ src/components/hourly-weather-row.tsx
@@ -18,34 +18,36 @@ export default function HourlyWeatherRow({ data }: HourlyWeatherRowProps) {
           const temperature = getParameterValue(item.parameters, "t");
           const windDirection = getParameterValue(item.parameters, "wd");
           const windSpeed = getParameterValue(item.parameters, "ws");
+          const hour24 = new Date(item.hour).toLocaleString("en-US", {
+            hour: "numeric",
+            hour12: false,
+            minute: "numeric",
+          });
 
           return (
             <div key={index} className="text-center cursor-default">
-              <p className="text-xs font-bold text-text-muted">
-                {new Date(item.hour).toLocaleString("en-US", {
-                  hour: "numeric",
-                  hour12: false,
-                  minute: "numeric",
-                })}
-              </p>
-              <p className="font-bold">{temperature || NaN}°</p>
-              {weatherSymbol && (
-                <span className="text-text-muted flex justify-center text-2xl">
-                  <WeatherIcon weatherSymbol={weatherSymbol} hour={hour} />
-                </span>
-              )}
-              <span className={`text-text-muted flex justify-center text-2xl`}>
-                <WiDirectionUp
-                  style={{
-                    rotate: `${windDirection}deg`,
-                  }}
-                  title={`${windDirection} degrees`}
-                  aria-label={`Wind direction`}
-                />
-              </span>
-              <span className="text-text-muted flex justify-center text-xs">
-                {windSpeed} m/s
-              </span>
+              <p className="text-xs font-bold text-text-muted">{hour24}</p>
+              {/* Parameters */}
+              <div className="flex flex-col">
+                <div className="font-bold">{temperature || NaN}°</div>
+                {weatherSymbol && (
+                  <div className="text-text-muted flex justify-center text-2xl">
+                    <WeatherIcon weatherSymbol={weatherSymbol} hour={hour} />
+                  </div>
+                )}
+                <div className={`text-text-muted flex justify-center text-2xl`}>
+                  <WiDirectionUp
+                    style={{
+                      rotate: `${windDirection}deg`,
+                    }}
+                    title={`${windDirection} degrees`}
+                    aria-label={`Wind direction`}
+                  />
+                </div>
+                <div className="text-text-muted flex justify-center text-xs">
+                  {windSpeed} m/s
+                </div>
+              </div>
             </div>
           );
         })}
